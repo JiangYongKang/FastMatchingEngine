db291932f71eb2e1dbfb2f89b5b52c123ea12df2
vincent <vincent4502237@gmail.com>
2020-01-30 14:42:41 +0800

    忽略重复订单

--- src/test/java/com/vincent/goblin/exchange/tests/OrderBucketTests.java
+++ src/test/java/com/vincent/goblin/exchange/tests/OrderBucketTests.java
@@ -23,11 +23,17 @@ import com.vincent.goblin.exchange.engine.OrderBucket;
 import com.vincent.goblin.exchange.model.Order;
 import com.vincent.goblin.exchange.model.OrderAction;
 import com.vincent.goblin.exchange.model.OrderCategory;
+import com.vincent.goblin.exchange.model.Trade;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class OrderBucketTests {
@@ -58,7 +64,7 @@ public class OrderBucketTests {
     @Test
     public void shouldMatchOrders() {
         Order bidOrder = new Order(10, 9012L, 6L, OrderAction.BID, OrderCategory.LIMIT);
-        bidOrderBucket.create(bidOrder);
+        List<Trade> trades = bidOrderBucket.create(bidOrder);
         Assert.assertEquals(OrderBook.BID_DEPTH.get(bidOrder.getPrice()).longValue(), 1L);
         Assert.assertEquals(OrderBook.ASK_ORDERS.size(), 1);
         Assert.assertEquals(OrderBook.ASK_ORDERS.first().getPrice().longValue(), 9011L);
@@ -68,6 +74,7 @@ public class OrderBucketTests {
         Assert.assertEquals(OrderBook.BID_ORDERS.last().getAmount().longValue(), 1L);
         Assert.assertEquals(OrderBook.ASK_DEPTH.get(9013L).longValue(), 0L);
         Assert.assertEquals(OrderBook.ASK_DEPTH.get(9012L).longValue(), 0L);
+        Assert.assertEquals(trades.size(), 2);
     }
 
     @Test
@@ -86,14 +93,24 @@ public class OrderBucketTests {
 
     @Test
     public void shouldRemoveOrder() {
-        Order bidOrder = new Order(10, 9015L, 1L, OrderAction.BID, OrderCategory.LIMIT);
+        Assert.assertEquals(OrderBook.BID_ORDERS.size(), 3);
+        Assert.assertEquals(OrderBook.BID_DEPTH.get(9017L).longValue(), 1L);
+        Order bidOrder = new Order(10, 9017L, 1L, OrderAction.BID, OrderCategory.LIMIT);
         bidOrderBucket.create(bidOrder);
+        Assert.assertEquals(OrderBook.BID_DEPTH.get(9017L).longValue(), 2L);
+        Assert.assertEquals(OrderBook.BID_ORDERS.size(), 4);
         bidOrderBucket.remove(bidOrder.getSn());
+        Assert.assertEquals(OrderBook.BID_DEPTH.get(9017L).longValue(), 1L);
+        Assert.assertEquals(OrderBook.BID_ORDERS.size(), 3);
+        Assert.assertEquals(OrderBook.ASK_DEPTH.get(9013L).longValue(), 2L);
+        Assert.assertEquals(OrderBook.ASK_ORDERS.size(), 3);
         Order askOrder = new Order(10, 9013L, 1L, OrderAction.ASK, OrderCategory.LIMIT);
         askOrderBucket.create(askOrder);
+        Assert.assertEquals(OrderBook.ASK_DEPTH.get(9013L).longValue(), 3L);
+        Assert.assertEquals(OrderBook.ASK_ORDERS.size(), 4);
         askOrderBucket.remove(askOrder.getSn());
+        Assert.assertEquals(OrderBook.ASK_DEPTH.get(9013L).longValue(), 2L);
         Assert.assertEquals(OrderBook.ASK_ORDERS.size(), 3);
-        Assert.assertEquals(OrderBook.BID_ORDERS.size(), 3);
     }
 
     @Test
@@ -116,6 +133,20 @@ public class OrderBucketTests {
             askOrderBucket.create(new Order(RandomUtils.nextInt(1, 1000000), RandomUtils.nextLong(8000, 10000), RandomUtils.nextLong(1, 10), OrderAction.ASK, OrderCategory.LIMIT));
             bidOrderBucket.create(new Order(RandomUtils.nextInt(1, 1000000), RandomUtils.nextLong(8000, 10000), RandomUtils.nextLong(1, 10), OrderAction.BID, OrderCategory.LIMIT));
         }
+        log.info("ASK ORDERS SIZE: {}", OrderBook.ASK_ORDERS.size());
+        log.info("BID ORDERS SIZE: {}", OrderBook.BID_ORDERS.size());
+    }
+
+    @Test
+    public void OrderMatchPressureTest2() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 100000; i++) {
+            executorService.execute(() -> {
+                askOrderBucket.create(new Order(RandomUtils.nextInt(1, 1000000), RandomUtils.nextLong(8000, 10000), RandomUtils.nextLong(1, 10), OrderAction.ASK, OrderCategory.LIMIT));
+                bidOrderBucket.create(new Order(RandomUtils.nextInt(1, 1000000), RandomUtils.nextLong(8000, 10000), RandomUtils.nextLong(1, 10), OrderAction.BID, OrderCategory.LIMIT));
+            });
+        }
+        executorService.awaitTermination(2, TimeUnit.SECONDS);
         log.info("ASK ORDERS SIZE: {}", OrderBook.ASK_ORDERS.size());
         log.info("BID ORDERS SIZE: {}", OrderBook.BID_ORDERS.size());
     }
